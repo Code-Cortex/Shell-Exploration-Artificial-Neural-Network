250e72f18a09167b0dcf2d74bd017ecf9a3c2559
GooseXP <zka719@gmail.com>
2020-11-25 22:28:19 -0600

    Update ShEANN.py

--- ShEANN.py
+++ ShEANN.py
@@ -146,8 +146,8 @@ while True:
         continue
     else:
         cmd_in = True
+    if done:
+        done = False
     inverse_model.save_weights(inv_weights_fname, overwrite=True)
     forward_model.save_weights(fwd_weights_fname, overwrite=True)
     agent.save_weights(agent_weights_fname, overwrite=True)
-    if done:
-        done = False
