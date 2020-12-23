0bb72f1f6a71b8c5e00fd706b8332331086fbfd2
GooseXP <zka719@gmail.com>
2020-12-23 13:49:10 -0600

    Update ShEANN.py

--- ShEANN.py
+++ ShEANN.py
@@ -139,7 +139,6 @@ while True:
     r_intr = (fwd_loss[0] ** 0.5) / 100
     reward = r_intr + env_reward
     agent.backward(reward, done)
-    clear_session()
     done = False
 
     enc_ascii = action + 32
@@ -151,3 +150,4 @@ while True:
     inverse_model.save_weights(inv_weights_fname, overwrite=True)
     forward_model.save_weights(fwd_weights_fname, overwrite=True)
     agent.save_weights(agent_weights_fname, overwrite=True)
+    clear_session()
