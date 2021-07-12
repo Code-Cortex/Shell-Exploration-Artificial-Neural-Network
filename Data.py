30002aebebba84b1a25143c1fe2a2e57a761096e
GooseXP <zka719@gmail.com>
2021-07-12 09:30:59 -0500

    Update Data.py

--- Data.py
+++ Data.py
@@ -88,7 +88,7 @@ def create_model(nb_actions):
 def model_mutate(weights):
     for i in range(len(weights)):
         for j in range(len(weights[i])):
-            if random.uniform(0, 1) > .96:
+            if random.uniform(0, 1) > .85:
                 change = random.uniform(-.01, .01)
                 weights[i][j] += change
     return weights
