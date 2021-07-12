5f0ac4c92a5fb9c7c0e1f983da55b252600ce620
GooseXP <zka719@gmail.com>
2021-07-12 09:29:50 -0500

    Update Data.py

--- Data.py
+++ Data.py
@@ -88,8 +88,8 @@ def create_model(nb_actions):
 def model_mutate(weights):
     for i in range(len(weights)):
         for j in range(len(weights[i])):
-            if random.uniform(0, 1) > .85:
-                change = random.uniform(-.5, .5)
+            if random.uniform(0, 1) > .96:
+                change = random.uniform(-.01, .01)
                 weights[i][j] += change
     return weights
 
