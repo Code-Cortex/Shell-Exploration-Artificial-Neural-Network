be48d88049c1b5268a56534e6b245d090c84a8af
GooseXP <zka719@gmail.com>
2021-07-16 11:41:54 -0500

    Update Data.py

--- Data.py
+++ Data.py
@@ -107,7 +107,7 @@ def model_mutate(weights):
     for i in range(len(weights)):
         for j in range(len(weights[i])):
             if random.uniform(0, 1) > mutation_rate:
-                change = random.uniform(-.01, .01)
+                change = random.uniform(-.1, .1)
                 weights[i][j] += change
     return weights
 
